5948d318a25aca1a6bf355babc90293c0308e4e9
Patrik Körn <patrik.koern@gmail.com>
2024-10-13 22:40:33 +0200

    make about window non dockable

--- src/windows/about.c
+++ src/windows/about.c
@@ -2,7 +2,7 @@
 
 void AboutWindow(bool *p_open)
 {
-    if(igBegin("About", p_open, ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoCollapse))
+    if(igBegin("About", p_open, ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoDocking))
     {
         igText("Editor2\nA map editor for the WeekRPG project\nMade by Novalis");
     }
